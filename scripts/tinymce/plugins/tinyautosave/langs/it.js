78bde61cd77df565fd11e6d811e0465a6623825f
Diego La Monica <me@diegolamonica.info>
2010-11-18 22:52:33 +0100

    Aggiornata libreria TinyMCE

---
+++ scripts/tinymce/plugins/tinyautosave/langs/it.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('it.tinyautosave',{
+restore_content: "Ripristina il contenuto",
+no_content: "Non c'è contenuto da ripristinare",
+warning_message: "Ripristinando il contenuto salvato perderai eventuali modifiche apportate. Sei sicuro di voler procedere?"
+});
